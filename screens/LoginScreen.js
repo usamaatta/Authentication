934091749e5444f7b634abee4d9c9c0ed988a138
Your Name <you@example.com>
2020-09-19 07:30:31 +0500

    new changing in login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,11 +4,28 @@ import firebase from 'firebase';
 import * as Google from 'expo-google-app-auth';
 import {Asset} from 'expo-asset';
 import {AppLoading} from 'expo';
-//import Animated from 'react-native-reanimated';
-//import {TapGestureHandler,State} from 
-//'react-native-gesture-handler';
+import Animated,{Easing} from 'react-native-reanimated';
+import {TapGestureHandler,State} from 
+'react-native-gesture-handler';
 
 
+
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+  Extrapolate
+} = Animated;
 
 const {width,height}=Dimensions.get('window');
 
@@ -21,7 +38,40 @@ function cacheImages(images) {
     }
   });
 }
+
+// react native reanimated
+
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0)
+  };
+
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease)
+  };
+
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock)
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    state.position
+  ]);
+}
+
 class LoginScreen extends Component {
+
 
   isUserEqual = (googleUser, firebaseUser) => {
     //console.log ("this is login from firebase1")
@@ -127,10 +177,37 @@ class LoginScreen extends Component {
   // Assets and front-end working start here
       constructor(){
         super()
+        this.buttonOpacity = new Value(1);
+        this.onStateChange = event([
+          {
+            nativeEvent: ({state})=>block([
+              cond(eq(state,State.END),set(this.buttonOpacity,runTiming(new Clock(), 1, 0)))
+            ])
+          }
+        ])
+
+
         this.state={
           isReady:false
         }
+
+
+        this.buttonY = interpolate(this.buttonOpacity, {
+          inputRange: [0, 1],
+          outputRange: [100, 0],
+          extrapolate: Extrapolate.CLAMP
+        });
+    
+        this.bgY = interpolate(this.buttonOpacity, {
+          inputRange: [0, 1],
+          outputRange: [-height / 3, 0],
+          extrapolate: Extrapolate.CLAMP
+        });
+
       }
+
+
+
       async _loadAssetsAsync() {
         const imageAssets = cacheImages([
 
@@ -153,32 +230,35 @@ class LoginScreen extends Component {
       <View style={{flex: 1,backgroundColor:'white',
       justifyContent:'flex-end' }}>
 
-        <View style={{...StyleSheet.absoluteFill}}>
+
+
+        <Animated.View style={{...StyleSheet.absoluteFill,transform: [{ translateY: this.bgY }]}}>
           <Image 
           source={require('./assets/bg.jpg')}
           style={{flex:1, height: null,width: null}}
           />
-        </View>
+        </Animated.View>
         <View style={{height:height/3,justifyContent:'center'}}>
-        <View style={{...styles.button,backgroundColor:'#00000050'}}>
-          <Text style={{fontSize:20, fontWeight:'bold',color:'white'}} onPress={() => this.signInWithGoogleAsync()} > SIGN IN WITH GOOGLE
+
+
+        <TapGestureHandler onHandlerStateChange = {this.onStateChange}>
+
+        <Animated.View style={{...styles.button,opacity:this.buttonOpacity, transform: [{ translateY: this.buttonY }],backgroundColor:'#00000050'}}>
+          <Text style={{fontSize:20, fontWeight:'bold',color:'white'}}  > SIGN IN 
           </Text>
-          
-        </View>
-        <View style={{...styles.button,backgroundColor:'#2E71DC'}}>
-          <Text style={{fontSize:20, fontWeight:'bold',color:'white'}} >
-             SIGN IN WITH Facebook
+        </Animated.View> 
+
+        </TapGestureHandler>
+
+
+        <Animated.View style={{...styles.button,opacity:this.buttonOpacity, transform: [{ translateY: this.buttonY }],backgroundColor:'white'}}>
+          <Text style={{fontSize:20, fontWeight:'bold',color:'black'}} onPress={() => this.signInWithGoogleAsync()} >
+             SIGN IN WITH Google
           </Text> 
-        </View>
+        </Animated.View>
 
         </View>
      
-        {/* <Button
-          title=" Sign In With Google"
-         onPress={() => this.signInWithGoogleAsync()}
-          //onPress={() => console.log ("this is button")}
-          
-        /> */}
       </View>
     );
   }
